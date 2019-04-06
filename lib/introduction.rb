930c5feaa9dd63fa14283a6f7035c2b8d454799b
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-04-06 23:01:09 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,3 @@
-def introduction_with_language(name="Dan", language="ember.js")
+def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I'm learning to programm #{language}."
 end
